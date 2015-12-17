35357cc894282042e75416cc2f043819c87b8e85
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-16 19:17:23 -0700

    Rename duplicate user variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
   def self.from_omniauth(user_info)
     raw_info = user_info["extra"]["raw_info"]
 
-    user = self.where(third_party_id: user_info["uid"]).first_or_create do |user|
-      user.name                = raw_info["name"]
-      user.nickname            = raw_info["screen_name"]
-      user.access_token        = user_info["credentials"]["token"]
-      user.access_token_secret = user_info["credentials"]["secret"]
-      user.followers_count     = raw_info["followers_count"]
-      user.following_count     = raw_info["friends_count"]
-      user.tweets_count        = raw_info["statuses_count"]
-      user.profile_image_url   = raw_info["profile_image_url"]
+    user = self.where(third_party_id: user_info["uid"]).first_or_create do |new_user|
+      new_user.name                = raw_info["name"]
+      new_user.nickname            = raw_info["screen_name"]
+      new_user.access_token        = user_info["credentials"]["token"]
+      new_user.access_token_secret = user_info["credentials"]["secret"]
+      new_user.followers_count     = raw_info["followers_count"]
+      new_user.following_count     = raw_info["friends_count"]
+      new_user.tweets_count        = raw_info["statuses_count"]
+      new_user.profile_image_url   = raw_info["profile_image_url"]
     end
 
     check_for_updates(user, raw_info)
